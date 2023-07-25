cd89073bcfe4ecb6d9e53bf974eddb7091620246
KyoungRyun Kim <krkim1913@gmail.com>
2023-07-25 10:36:12 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/anywayclear/mokumoku/MokuMokuApplication.java
@@ -0,0 +1,13 @@
+package com.anywayclear.mokumoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokuMokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokuMokuApplication.class, args);
+	}
+
+}
