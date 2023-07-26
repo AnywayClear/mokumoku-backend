4b354eb34c5d23fc782fd14f4917eb71e93f5955
KyoungRyun Kim <krkim1913@gmail.com>
2023-07-26 11:34:05 +0900

    chore: to yml, 패키지명 변경

--- src/main/java/com/anywayclear/MokuMokuApplication.java
+++ src/main/java/com/anywayclear/MokuMokuApplication.java
@@ -1,4 +1,4 @@
-package com.anywayclear.mokumoku;
+package com.anywayclear;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MokuMokuApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MokuMokuApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MokuMokuApplication.class, args);
+    }
 }
