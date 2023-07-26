c73e7149841d1a465b5ccb720484870cb6c90aa5
klkim1913 <krkim1913@gmail.com>
2023-07-26 12:47:21 +0900

    test: inteli push test

--- src/main/java/com/anywayclear/MokuMokuApplication.java
+++ src/main/java/com/anywayclear/MokuMokuApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MokuMokuApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MokuMokuApplication.class, args);
     }
