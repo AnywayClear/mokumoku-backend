baaae73344ad0442aeb6c2a6418e9f79438d8c91
flowerdonk <dbwls5140@gmail.com>
2023-07-28 15:11:35 +0900

    Feat: 구독 컨트롤러 추가

---
+++ src/main/java/com/anywayclear/controller/SubscribeController.java
@@ -0,0 +1,10 @@
+package com.anywayclear.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class SubscribeController {
+
+}
