83e6639076472ec90dec2830da3b557f7aac5008
flowerdonk <dbwls5140@gmail.com>
2023-07-30 04:17:48 +0900

    Feat: 닉네임으로 구독 객체 가져오기

--- src/main/java/com/anywayclear/controller/SubscribeController.java
+++ src/main/java/com/anywayclear/controller/SubscribeController.java
@@ -1,10 +1,24 @@
 package com.anywayclear.controller;
 
+import com.anywayclear.dto.response.SubscribeResponseList;
+import com.anywayclear.service.SubscribeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/api/subscribes")
 public class SubscribeController {
+    private final SubscribeService subscribeService;
 
+    public SubscribeController(SubscribeService subscribeService) {
+        this.subscribeService = subscribeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<SubscribeResponseList> getSubscribeList(@RequestParam(name = "nickname") String nickname) {
+        return ResponseEntity.ok(subscribeService.getSubscribeList(nickname));
+    }
 }
--- src/main/java/com/anywayclear/service/SubscribeService.java
+++ src/main/java/com/anywayclear/service/SubscribeService.java
@@ -2,16 +2,23 @@ package com.anywayclear.service;
 
 import com.anywayclear.dto.request.SubscribeCreateRequest;
 import com.anywayclear.dto.response.SubscribeResponse;
+import com.anywayclear.dto.response.SubscribeResponseList;
+import com.anywayclear.entity.Member;
 import com.anywayclear.entity.Subscribe;
+import com.anywayclear.repository.MemberRepository;
 import com.anywayclear.repository.SubscribeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SubscribeService {
     private final SubscribeRepository subscribeRepository;
+    private final MemberRepository memberRepository;
 
-    public SubscribeService(SubscribeRepository subscribeRepository) {
+    public SubscribeService(SubscribeRepository subscribeRepository, MemberRepository memberRepository) {
         this.subscribeRepository = subscribeRepository;
+        this.memberRepository = memberRepository;
     }
 
     public Long createSubscribe(SubscribeCreateRequest request) {
@@ -21,5 +28,16 @@ public class SubscribeService {
     public SubscribeResponse getSubscribe(Long id) {
         Subscribe subscribe = subscribeRepository.findById(id).orElseThrow(() -> new RuntimeException());
         return SubscribeResponse.toResponse(subscribe);
+    }
+
+    public SubscribeResponseList getSubscribeList(String nickname) {
+        Member member = memberRepository.findByNickname(nickname).orElseThrow(() -> new RuntimeException("해당 닉네임의 유저가 없습니다."));
+        List<Subscribe> subscribeList;
+        if (member.getRole() == "ROLE_SELLER") { // 판매자 일 경우
+            subscribeList = subscribeRepository.findAllBySeller(member);
+        } else { // 소비자 일 경우
+            subscribeList = subscribeRepository.findAllByConsumer(member);
+        }
+        return new SubscribeResponseList(subscribeList);
     }
 }
