6e7cf9d1fe299a8e752835a5d961f42233f51905
ah9mon <eomgyeol97@gmail.com>
2023-07-31 02:52:09 +0900

    feat: 로그인 성공 시 JWT 생성 및 front로 redirect 기능 추
    - login 및 회원가입 후 로직 실행되는OAuth2AthenticationSuccessHandler 추가
    - JwtProvider에 JWT생성 기능 추가

--- src/main/java/com/anywayclear/config/SecurityConfig.java
+++ src/main/java/com/anywayclear/config/SecurityConfig.java
@@ -1,14 +1,17 @@
 package com.anywayclear.config;
 
 import com.anywayclear.config.oauth.CustumOAuth2UserService;
+import com.anywayclear.config.oauth.OAuth2AuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
@@ -17,15 +20,26 @@ public class SecurityConfig {
     @Autowired
     private CustumOAuth2UserService custumOAuth2UserService;
 
+    @Autowired
+    private OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+
+    @Autowired
+    private CorsFilter corsFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.cors(Customizer.withDefaults()) // 기본 CORS 설정 적용
+
+        httpSecurity
+                .addFilterBefore(corsFilter, CorsFilter.class) // CorsFilter를 SecurityFilterChain 앞에 추가
                 .csrf().disable() // CSRF 비활성화
+                .formLogin().disable() // spring security에서 제공하는 login form 비활성화
                 .authorizeHttpRequests(authorize -> authorize
                         .antMatchers(HttpMethod.OPTIONS).permitAll() // OPTIONS 메서드는 모두 허용
                         .anyRequest().permitAll() // 모든 요청 권한 허용 (추후 권한 설정해야함)
                 )
                 .oauth2Login(oauth2Login -> oauth2Login
+//                        .loginPage("/frontLoginPage") // OAuth 2.0 로그인을 처리할 때, 인증되지 않은 사용자를 전달할 로그인 페이지를 지정
+                        .successHandler(oAuth2AuthenticationSuccessHandler)
                         .userInfoEndpoint()
                         .userService(custumOAuth2UserService)
                 );
