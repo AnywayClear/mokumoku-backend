e292e0c875aefcac87407a8dc5e84f88d5ed2531
klkim1913 <krkim1913@gmail.com>
2023-07-31 15:16:49 +0900

    Feat: 못난이 등록시 등록 수만큼 경매건 생성하도록 수정

--- src/main/java/com/anywayclear/entity/Auction.java
+++ src/main/java/com/anywayclear/entity/Auction.java
@@ -1,11 +1,15 @@
 package com.anywayclear.entity;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Auction {
     @Id
