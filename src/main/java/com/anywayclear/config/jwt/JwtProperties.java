d2450fbc2c6cfd5c9fdccfe1aae4dc41e038432b
ah9mon <eomgyeol97@gmail.com>
2023-08-02 20:08:03 +0900

    chore: Added JwtProperties.java to .gitignore

--- src/main/java/com/anywayclear/config/jwt/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package com.anywayclear.config.jwt;
-
-public interface JwtProperties {
-    String SECRET_KEY = "kyunwangJwt";
-    int EXPIRATION_TIME = 1000 * 60 * 60 * 24; // 24시간
-    String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING = "Authorization";
-}
