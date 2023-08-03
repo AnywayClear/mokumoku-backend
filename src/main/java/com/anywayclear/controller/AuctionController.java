3b1fee81bc158dd8d729e8274fa3845c706eb916
klkim1913 <krkim1913@gmail.com>
2023-08-03 14:24:09 +0900

    Feat: synchronized 테스트

--- src/main/java/com/anywayclear/controller/AuctionController.java
+++ src/main/java/com/anywayclear/controller/AuctionController.java
@@ -4,7 +4,6 @@ import com.anywayclear.dto.request.BiddingRequest;
 import com.anywayclear.dto.response.BiddingResponse;
 import com.anywayclear.service.AuctionService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -22,6 +21,16 @@ public class AuctionController {
     public ResponseEntity<BiddingResponse> bidding(
             @PathVariable("auction-id") long auctionId,
             @RequestBody BiddingRequest request) {   // 인증정보 받기 추가 예정
-        return ResponseEntity.ok(auctionService.Bidding(auctionId,request));
+        return ResponseEntity.ok(auctionService.Bidding(auctionId, request));
+    }
+
+    /*
+    * 테스트용
+    */
+    @PostMapping("/{auction-id}/test")
+    public ResponseEntity<BiddingResponse> test(
+            @PathVariable("auction-id") long auctionId,
+            @RequestBody BiddingRequest request){
+        return ResponseEntity.ok(auctionService.autoBidding(auctionId, request));
     }
 }
--- src/main/java/com/anywayclear/service/AuctionService.java
+++ src/main/java/com/anywayclear/service/AuctionService.java
@@ -19,7 +19,7 @@ public class AuctionService {
     }
 
     @Transactional
-    public BiddingResponse Bidding(long auctionId,BiddingRequest request) {
+    public synchronized BiddingResponse Bidding(long auctionId, BiddingRequest request) {
         Auction auction = auctionRepository.findById(auctionId).orElseThrow(() -> new CustomException(INVALID_AUCTION_ID));
         if (request.getPrice() < auction.getPrice() + 100) { // 가격 기준 정해지면 수정할 로직
             throw new CustomException(INVALID_PRICE);
@@ -27,6 +27,19 @@ public class AuctionService {
         auction.setPrice(request.getPrice());   // 트랜잭션 내에서 변경시 자동 update
         return BiddingResponse.builder()
                 .userId(request.getUserId())    // 유저 아이디는 요청한 유저에게 자동으로 받기로 수정 예정
+                .price(request.getPrice())
+                .build();
+    }
+
+    /**
+     * test용
+     */
+    @Transactional
+    public BiddingResponse autoBidding(long auctionId, BiddingRequest request) {
+        Auction auction = auctionRepository.findById(auctionId).orElseThrow(() -> new CustomException(INVALID_AUCTION_ID));
+        auction.setPrice(auction.getPrice() + 100);
+        return BiddingResponse.builder()
+                .userId(request.getUserId())
                 .price(request.getPrice())
                 .build();
     }
