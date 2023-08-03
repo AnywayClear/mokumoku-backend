c3dfe786e840836dfac461a9de89e4c6f3e3cf09
ah9mon <eomgyeol97@gmail.com>
2023-08-03 23:02:31 +0900

    fix: JWT 리턴방식 변경 및 예외처리 추가
    - 로그인 성공시 클라이언트 페이지로 쿼리파라미터로 JWT 전송
    - OAuth2AuthenticationFailureHandler 추가하여 로그인 실패시 exception 리턴
    - JwtConfig에 리다이렉트 주소 추가
    - JwtAccessDeniedHandler로 요청거부 예외처리

--- src/main/java/com/anywayclear/config/SecurityConfig.java
+++ src/main/java/com/anywayclear/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.anywayclear.config;
 
+import com.anywayclear.config.jwt.JwtAccessDeniedHandler;
 import com.anywayclear.config.jwt.JwtAuthorizationFilter;
 import com.anywayclear.config.oauth.CustumOAuth2UserService;
 import com.anywayclear.config.oauth.OAuth2AuthenticationSuccessHandler;
+import com.anywayclear.config.oauth.OAuth2AuthenticationFailureHandler;
 import com.anywayclear.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +17,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -26,33 +30,55 @@ public class SecurityConfig {
 
     private final CustumOAuth2UserService custumOAuth2UserService;
     private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
     private final CorsFilter corsFilter;
+    private final JwtConfig jwtConfig;
     private final MemberRepository memberRepository;
     private final AuthenticationConfiguration authenticationConfiguration;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-
-        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 방식 인증 비활성화
+        // 인증 방식 설정
+        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .addFilter(corsFilter) // CorsFilter를 SecurityFilterChain 앞에 추가
-                .csrf().disable()
-                .formLogin().disable() // spring security에서 제공하는 login form 비활성화
-                .httpBasic().disable() // Basic 방식 인증 인가 비활성화
+                .formLogin().disable()
+                .httpBasic().disable();
+
+        // Cors 필터 추가, CSRF 비활성화
+        httpSecurity.addFilterBefore(corsFilter, BasicAuthenticationFilter.class)
+                .csrf().disable();
+
+        // JWT Authorization 필터 추가
+        httpSecurity
                 .addFilter(jwtAuthorizationFilter())
+                .exceptionHandling().accessDeniedHandler(jwtAccessDeniedHandler());
+
+        // 요청 권한 설정
+        httpSecurity
                 .authorizeHttpRequests(authorize -> authorize
                         .antMatchers(HttpMethod.OPTIONS).permitAll() // OPTIONS 메서드는 모두 허용
                         .anyRequest().permitAll() // 모든 요청 권한 허용 (추후 권한 설정해야함)
-                )
+                );
+
+        // OAuth2 로그인 설정
+        httpSecurity
                 .oauth2Login(oauth2Login -> oauth2Login
-//                        .loginPage("https://mokumoku-git-develop-mokumoku.vercel.app") // OAuth 2.0 로그인을 처리할 때, 인증되지 않은 사용자를 전달할 로그인 페이지를 지정
-                                .successHandler(oAuth2AuthenticationSuccessHandler)
+                                // 로그인 페이지 지정
+//            .loginPage("https://mokumoku-git-develop-mokumoku.vercel.app")
                                 .userInfoEndpoint()
                                 .userService(custumOAuth2UserService)
-                ).headers().frameOptions().disable(); // h2사용을 위해 임시 설정
+                                .and()
+                                .successHandler(oAuth2AuthenticationSuccessHandler)
+                                .failureHandler(oAuth2AuthenticationFailureHandler)
+                );
 
-        return httpSecurity.build(); // 설정된 HttpSecurity를 SecurityFilterChain으로 반환
+        // H2 사용을 위한 설정
+        httpSecurity
+                .headers().frameOptions().disable();
+
+        return httpSecurity.build();
     }
+
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
@@ -60,7 +86,13 @@ public class SecurityConfig {
 
     @Bean
     public JwtAuthorizationFilter jwtAuthorizationFilter() throws Exception {
-        return new JwtAuthorizationFilter(authenticationManagerBean(), memberRepository);
+        return new JwtAuthorizationFilter(authenticationManagerBean(), memberRepository, jwtConfig);
+    }
+
+    @Bean
+    public AccessDeniedHandler jwtAccessDeniedHandler() {
+        // 커스텀 예외 처리기를 반환하는 빈 정의
+        return new JwtAccessDeniedHandler();
     }
 }
 
--- src/main/java/com/anywayclear/config/oauth/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/anywayclear/config/oauth/OAuth2AuthenticationSuccessHandler.java
@@ -3,6 +3,8 @@ package com.anywayclear.config.oauth;
 import com.anywayclear.config.JwtConfig;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseCookie;
@@ -12,39 +14,55 @@ import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
+
+@Log4j2
 @Component
+@RequiredArgsConstructor
 public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    @Autowired
-    private JwtConfig jwtConfig;
-    private RedirectStrategy redirectStratgy = new DefaultRedirectStrategy();
+    private final JwtConfig jwtConfig;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
 
         System.out.println("OAuth2AuthenticationSuccessHandler : 로그인 성공");
+
+        if (response.isCommitted()) {
+            log.debug("Response has already been committed");
+            return;
+        }
+
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
 
-        String jwtToken = JWT.create()
+        String accessToken = createToken(oAuth2User);
+        System.out.println("accessToken = " + accessToken);
+
+        String redirectUrl = createRedirectUrl(accessToken);
+
+        getRedirectStrategy().sendRedirect(request,response,redirectUrl);
+    }
+
+    private String createRedirectUrl(String accessToken) {
+        String redirectUrl = jwtConfig.getSuccess();
+        redirectUrl = UriComponentsBuilder.fromUriString(redirectUrl)
+                .queryParam("accessToken", accessToken)
+                .build().toUriString();
+        return redirectUrl;
+    }
+
+    private String createToken(OAuth2User oAuth2User) {
+        String accessToken = JWT.create()
                 .withSubject("mokumoku")
                 .withExpiresAt(new Date(System.currentTimeMillis() + (1000 * 60 * 60 * 24)))
                 .withClaim("userId", (String) oAuth2User.getAttributes().get("userId"))
                 .withClaim("role", (String) oAuth2User.getAttributes().get("role"))
                 .sign(Algorithm.HMAC512(jwtConfig.getKey()));
-        System.out.println("jwtToken = " + jwtToken);
-        ResponseCookie cookie = ResponseCookie.from("jwtToken", jwtToken)
-                        .path("/")
-//                        .secure(true)
-                        .httpOnly(true)
-                        .build();
-        response.addHeader(jwtConfig.getHeader(), jwtConfig.getPrefix() + " " + jwtToken);
-        response.addHeader("Set-Cookie", cookie.toString());
-        String redirectUrl = "http://localhost:3000";
-        redirectStratgy.sendRedirect(request, response, redirectUrl);
+        return accessToken;
     }
 }
