c6f92efd57d8e5023c75b8ce11ff32e0af6452e9
klkim1913 <krkim1913@gmail.com>
2023-08-04 16:51:32 +0900

    Test: test 주석처리 풀고 수정

--- src/test/java/com/anywayclear/repository/MemberTest.java
+++ src/test/java/com/anywayclear/repository/MemberTest.java
@@ -1,38 +1,33 @@
-//package com.anywayclear.repository;
-//
-//import com.anywayclear.entity.Member;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import javax.transaction.Transactional;
-//
-//@Transactional
-//@SpringBootTest
-//public class MemberTest {
-//
-//    @Autowired
-//    MemberRepository memberRepository;
-//
-//    @Autowired
-//    MemberStatusRepository memberStatusRepository;
-//
-//    @Test
-//    void SignUpTest() {
-//        Member member = new Member();
-//        member.setId("S000001");
-//        member.setNickname("flowerdonk");
-////        member.setPhone_number("01085025150");
-//        member.setDescription("싱싱한 딸기");
-////        member.setCompany_registration_number("123456");
-////        member.setCompany_address("Incheon");
-//        Member savedMember = memberRepository.save(member);
-//
-//        MemberStatus memberStatus = new MemberStatus();
-//        memberStatus.setMember(savedMember);
-//        MemberStatus savedMemberStatus = memberStatusRepository.save(memberStatus);
-//
-//        System.out.println("savedMember = " + savedMember);
-//        System.out.println("savedMemberStatus = " + savedMemberStatus);
-//    }
-//}
+package com.anywayclear.repository;
+
+import com.anywayclear.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@SpringBootTest
+public class MemberTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test
+    void SignUpTest() {
+        Member member = new Member();
+        member.setId("S000001");
+        member.setNickname("flowerdonk");
+//        member.setPhone_number("01085025150");
+        member.setDescription("싱싱한 딸기");
+//        member.setCompany_registration_number("123456");
+//        member.setCompany_address("Incheon");
+        member.setMemberStatus(true);
+        Member savedMember = memberRepository.save(member);
+
+
+        System.out.println("savedMember = " + savedMember);
+    }
+}
