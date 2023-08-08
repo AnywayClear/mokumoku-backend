7887654d1c19a198de6e3ad0e878d15b2d5de7fb
klkim1913 <49425719+klkim1913@users.noreply.github.com>
2023-08-08 14:50:02 +0900

    Feat: 동시성 처리 (#68)
    * Test: 비관적 락 사용
    
    * Test: 비관적 트랜 어노 위에
    
    * Test: 비관락 재시도 및 test 주석처리
    
    * Test: test 주석처리 풀고 수정
    
    * Test: Lock 제외하고 테스트
    
    * Test: PESSIMISTIC_WRITE 재시도
    
    * Test: PESSIMISTIC_READ로 변경
    
    * Test: Repository에 Lock
    
    * Test: 낙관적 락 적용
    
    * Test: 입찰 테스트코드 초안 임시저장
    
    * Feat: 동시성 처리

--- src/main/java/com/anywayclear/dto/request/BiddingRequest.java
+++ src/main/java/com/anywayclear/dto/request/BiddingRequest.java
@@ -1,10 +1,12 @@
 package com.anywayclear.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class BiddingRequest {
     private String userId;
     private int price;
