20d5b1254be4a50abc4259d1efce48f6398cae22
flowerdonk <dbwls5140@gmail.com>
2023-08-09 17:06:39 +0900

    Feat: SSE 기능 추가

---
+++ src/main/java/com/anywayclear/repository/SSERepository.java
@@ -0,0 +1,22 @@
+package com.anywayclear.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public interface SSERepository {
+    void put(String emitterId, SseEmitter sseEmitter);
+
+    Optional<SseEmitter> get(String key);
+
+    List<SseEmitter> getListByKeyPrefix(String keyPrefix);
+
+    List<String> getKeyListByKeyPrefix(String keyPrefix);
+
+    void remove(String key);
+}
