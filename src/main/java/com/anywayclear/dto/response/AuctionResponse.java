b45927d912fc6f8b34e99a018b9f7752dac9c7db
klkim1913 <krkim1913@gmail.com>
2023-08-09 14:33:05 +0900

    Fix: 경매 응답에 nickname, 갱신일 추가

--- src/main/java/com/anywayclear/dto/response/AuctionResponse.java
+++ src/main/java/com/anywayclear/dto/response/AuctionResponse.java
@@ -4,21 +4,29 @@ import com.anywayclear.entity.Auction;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class AuctionResponse {
     private final Long id;
     private final int price;
+    private final String nickname;
+    private final LocalDateTime updatedAt;
 
     @Builder
-    public AuctionResponse(Long id, int price) {
+    public AuctionResponse(Long id, int price, String nickname, LocalDateTime updatedAt) {
         this.id = id;
         this.price = price;
+        this.nickname = nickname;
+        this.updatedAt = updatedAt;
     }
 
     public static AuctionResponse toResponse(Auction auction) {
         return AuctionResponse.builder()
                 .id(auction.getId())
                 .price(auction.getPrice())
+                .nickname(auction.getNickname())
+                .updatedAt(auction.getUpdatedAt())
                 .build();
     }
 }
