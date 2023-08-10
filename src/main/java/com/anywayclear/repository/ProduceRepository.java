aa398e3e8cb6c3f8c9ef1541121a8a728225e72e
klkim1913 <krkim1913@gmail.com>
2023-08-10 16:44:08 +0900

    Feat: 자기 판매 농산물 및 못난이 검색 등

--- src/main/java/com/anywayclear/repository/ProduceRepository.java
+++ src/main/java/com/anywayclear/repository/ProduceRepository.java
@@ -1,5 +1,6 @@
 package com.anywayclear.repository;
 
+import com.anywayclear.entity.Member;
 import com.anywayclear.entity.Produce;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProduceRepository extends JpaRepository<Produce, Long> {
-    Page<Produce> findAllByStatusIn(List<Integer> statusNoList, Pageable pageable);
+    Page<Produce> findAllByStatusInAndNameContaining(List<Integer> statusNoList, Pageable pageable, String name);
 
+    Page<Produce> findAllBySellerAndStatusInAndNameContaining(Member seller,Pageable pageable,List<Integer> statusNoList,String name);
 }
