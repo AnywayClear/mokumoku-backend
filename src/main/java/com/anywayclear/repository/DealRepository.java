46ececeb5249bef71a228f1f6ac6685b23c1dd8e
ah9mon <eomgyeol97@gmail.com>
2023-08-14 11:21:35 +0900

    fix: deal response service
    - deal response에 review여부 및 deal_id 추가
    - deal list get API에 시간 범위로 거르는 기능 추가

--- src/main/java/com/anywayclear/repository/DealRepository.java
+++ src/main/java/com/anywayclear/repository/DealRepository.java
@@ -6,9 +6,23 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface DealRepository extends JpaRepository<Deal, Long> {
 
     Page<Deal> findAllByConsumer(Member member, Pageable pageable);
 
     Page<Deal> findAllBySeller(Member member, Pageable pageable);
+
+    Page<Deal> findAllByConsumerAndProduce_EndDateBetween(
+            Member member,
+            LocalDateTime startDate,
+            LocalDateTime endDate,
+            Pageable pageable);
+
+    Page<Deal> findAllBySellerAndProduce_EndDateBetween(
+            Member member,
+            LocalDateTime startDate,
+            LocalDateTime endDate,
+            Pageable pageable);
 }
