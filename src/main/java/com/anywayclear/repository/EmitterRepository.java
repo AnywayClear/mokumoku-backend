274c8fabdf425075f9aef96f9b3537fd081a0e8e
klkim1913 <krkim1913@gmail.com>
2023-08-16 15:59:24 +0900

    Feat: 경매완료 알림

--- src/main/java/com/anywayclear/repository/EmitterRepository.java
+++ src/main/java/com/anywayclear/repository/EmitterRepository.java
@@ -5,10 +5,12 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 @Repository
 public class EmitterRepository {
     private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
+    private final Map<String, Object> eventCache = new ConcurrentHashMap<>();
 
     /**
      *
@@ -19,10 +21,44 @@ public class EmitterRepository {
         emitters.put(id,emitter);
     }
 
+    public void saveEventCache(String id, Object event) {
+        eventCache.put(id, event);
+    }
+
+    public Map<String, SseEmitter> findAllStartWithById(String id) {
+        return emitters
+                .entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith(id))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+    public Map<String, Object> findAllEventCacheStartWithId(String id) {
+        return eventCache.entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith(id))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    public void deleteAllStartWithId(String id) {
+        emitters.forEach(
+                (key,emitter)->{
+                    if (key.startsWith(id)) {
+                        emitters.remove(key);
+                    }
+                }
+        );
+    }
     public void deleteById(String id) {
         emitters.remove(id);
     }
 
+    public void deleteAllEventCacheStartWithId(String id) {
+        eventCache.forEach(
+                (key, data) -> {
+                    if (key.startsWith(id)) {
+                        eventCache.remove(key);
+                    }
+                }
+        );
+    }
     public SseEmitter get(String id) {
         return emitters.get(id);
     }
