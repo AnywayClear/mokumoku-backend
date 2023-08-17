f7a7b54513f97f37b4e12830bc42aa25c0ac3fde
ah9mon <eomgyeol97@gmail.com>
2023-08-17 15:15:11 +0900

    feat: health check controller

---
+++ src/main/java/com/anywayclear/controller/StatusCheckController.java
@@ -0,0 +1,15 @@
+package com.anywayclear.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusCheckController {
+    @GetMapping("/health-check")
+    public ResponseEntity<Void> checkHealthStatus() {
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
